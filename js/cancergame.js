19e61a30aa72d81db93503734b783a49f4026aca
Greg Borenstein <greg.borenstein@gmail.com>
2014-03-17 00:04:27 -0400

    add treatment object and integrate it with game counter

--- js/cancergame.js
+++ js/cancergame.js
@@ -1,7 +1,31 @@
+Treatment = function(name, game, onCompletion){
+  this.name = "Doctor's Appointment";
+  this.length = 30;
+  this.startDay;
+  this.game = game;
+  this.onCompletion = onCompletion; // callback
+  this.started = false;
+}
+
+Treatment.prototype = {
+  start : function(){
+    this.startDay = this.game.getDaysRemaining();
+  },
+
+  isStarted : function(){
+      return this.startDay != undefined;
+  },
+
+  getRemainingDays : function(){
+    return this.length - (this.startDay - this.game.getDaysRemaining());
+  }
+}
+
 var Game = (function(){
   var daysLeft = 365 * 3;
   var secondsPerDay = 1;
   var clock;
+  var activeTreatment;
 
   function showClock(element){
     var years = Math.floor(daysLeft/365);
@@ -10,18 +34,26 @@ var Game = (function(){
   }
 
   return {
-    getDaysElapsed : function(){
-        return daysElapsed;
+
+    setActiveTreatment : function(treatment){
+        activeTreatment = treatment;
+    },
+
+    getDaysRemaining : function(){
+        return daysLeft;
     },
 
     startClock : function(){
       setInterval(function(){
         daysLeft = daysLeft - 1;
         showClock(clock);
+
+        if(activeTreatment && activeTreatment.isStarted() && activeTreatment.getRemainingDays() <= 0){
+          activeTreatment.onCompletion();
+          activeTreatment = null;
+        }
       }, 1000 * secondsPerDay);
     },
-
-
 
     setClockElement : function(element){
       clock = element;
